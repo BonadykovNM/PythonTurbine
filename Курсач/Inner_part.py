7af2edf3c0757211882ac0d0ed7333cee1653847
BonadykovNM <114513448+BonadykovNM@users.noreply.github.com>
2023-04-15 00:39:51 +0300

    добавил пробелы

--- Курсач/Inner_part.py
+++ Курсач/Inner_part.py
@@ -10,7 +10,7 @@ kPa = 10 ** 3
 unit = 1 / MPa
 to_kelvin = lambda x: x + 273.15 if x else None
 
-def real_point(p0,p_middle):
+def real_point(p0, p_middle):
         """
         Функция поиска действительного значения точек цикла
         :param float p0: Давление начальной точки в Па
@@ -26,7 +26,7 @@ def real_point(p0,p_middle):
         real_p_middle =  p_middle - delta_p_1
         return real_p0,real_p1t,real_p_middle
 
-def find_point0(p0,t0,p_middle):
+def find_point0(p0, t0, p_middle):
         """
         Функция поиска теоретического и действительного значения начальной точки 
         :param float p0: Давление начальной точки в Па
@@ -34,12 +34,12 @@ def find_point0(p0,t0,p_middle):
         :param float p_middle: Давление промежуточного перегрева в Па
         :return Tuple[IAPWS97, IAPWS97,IAPWS97]:Параметры теоретического значения начальной точки, Параметры действительного значения начальной точки
         """ 
-        real_points = real_point(p0,p_middle)
+        real_points = real_point(p0, p_middle)
         point_0t = gas(P =  p0 * unit, T=to_kelvin(t0))
         point_0 = gas(P= real_points[0] * unit, h=point_0t.h)
         return point_0t, point_0
     
-def find_point1(p0,t0,p_middle,internal_efficiency):  
+def find_point1(p0, t0, p_middle, internal_efficiency):  
         """
         Функция поиска теоретического и действительного значения первой точки
         :param float p0: Давление начальной точки в Па
@@ -48,15 +48,15 @@ def find_point1(p0,t0,p_middle,internal_efficiency):
         :param float internal_efficiency: КПД цилиндров турбины
         :return Tuple[IAPWS97, IAPWS97,IAPWS97]:Параметры теоретического значения первой точки, Параметры действительного значения первой точки
         """ 
-        real_points = real_point(p0,p_middle)
-        point0 = find_point0(p0,t0,p_middle)
+        real_points = real_point(p0, p_middle)
+        point0 = find_point0(p0, t0, p_middle)
         point_1t = gas(P= real_points[1] * unit, s= point0[0].s)
         hp_heat_drop = (point0[0].h - point_1t.h) *  internal_efficiency
         h_1 =  point0[1].h - hp_heat_drop
         point_1 = gas(P= real_points[1] * unit, h=h_1)
         return point_1t, point_1
     
-def find_point_middle(p_middle,t_middle,p0): 
+def find_point_middle(p_middle, t_middle, p0): 
         """
         Функция поиска теоретического и действительного значения точки пром перергрева 
         :param float p_middle: Давление промежуточного перегрева в Па
@@ -64,12 +64,12 @@ def find_point_middle(p_middle,t_middle,p0):
         :param float p0: Давление начальной точки в Па
         :return Tuple[IAPWS97, IAPWS97,IAPWS97]:Параметры теоретического значения точки пром. перегрева, Параметры действительного значения точки пром. перегрева
         """ 
-        real_points = real_point(p0,p_middle)
+        real_points = real_point(p0, p_middle)
         point_middle_t = gas(P= p_middle * unit, T=to_kelvin( t_middle))
         point_middle = gas(P= real_points[2] * unit, h=point_middle_t.h)
         return point_middle_t, point_middle
     
-def find_point2(p_middle,t_middle,p0,pk,internal_efficiency): 
+def find_point2(p_middle, t_middle, p0, pk, internal_efficiency): 
         """
         Функция поиска теоретического и действительного значения точки конденсатора
         :param float p_middle: Давление промежуточного перегрева в Па
@@ -79,14 +79,14 @@ def find_point2(p_middle,t_middle,p0,pk,internal_efficiency):
         :param float internal_efficiency: КПД цилиндров турбины
         :return Tuple[IAPWS97, IAPWS97,IAPWS97]:Параметры теоретического значения точки конденсатора, Параметры действительного значения точки конденсатора
         """
-        point_middle = find_point_middle(p_middle,t_middle,p0)
+        point_middle = find_point_middle(p_middle, t_middle, p0)
         point_2t = gas(P= pk * unit, s= point_middle[0].s)
         lp_heat_drop = (point_middle[0].h - point_2t.h) *  internal_efficiency
         h_2 = point_middle[1].h - lp_heat_drop
         point_2 = gas(P= pk * unit, h=h_2)
         return point_2t, point_2
 
-def find_point_water(pk,p_feed_water,t_feed_water):
+def find_point_water(pk, p_feed_water, t_feed_water):
         """
         Функция поиска параментров воды после конденсатора и памаметров питательной воды
         :param float pk: Давление точки конденсатора в Па
@@ -98,7 +98,7 @@ def find_point_water(pk,p_feed_water,t_feed_water):
         point_feed_water = gas(P= p_feed_water * unit, T=to_kelvin(t_feed_water))
         return point_k_water, point_feed_water
 
-def coeff (pk,p_feed_water,t_feed_water,internal_efficiency,p_middle,t_middle,p0):
+def coeff (pk, p_feed_water, t_feed_water, internal_efficiency, p_middle, t_middle, p0):
         """
         Функция расчета коэфициента
         :param float pk: Давление точки конденсатора в Па
@@ -110,8 +110,8 @@ def coeff (pk,p_feed_water,t_feed_water,internal_efficiency,p_middle,t_middle,p0
         :param float p0: Давление начальной точки в Па
         :return float: Значение коэфициента
         """  
-        point_water = find_point_water(pk,p_feed_water,t_feed_water)
-        point2 = find_point2(p_middle,t_middle,p0,pk,internal_efficiency)
+        point_water = find_point_water(pk, p_feed_water, t_feed_water)
+        point2 = find_point2(p_middle, t_middle, p0, pk, internal_efficiency)
         coeff = (( point_water[1].T -  point2[1].T) / (to_kelvin(374.2) -  point2[1].T))
         cf = 0.9  
         return cf
@@ -137,12 +137,12 @@ def complite_ksi_infinity(p0,
         :return float: Значение коэфициента ksi
         """ 
 
-        point2 = find_point2(p_middle,t_middle,p0,pk,internal_efficiency)
-        point_middle = find_point_middle(p_middle,t_middle,p0)
-        point_water = find_point_water(pk,p_feed_water,t_feed_water)
-        point0 = find_point0(p0,t0,p_middle)
-        point1 = find_point1(p0,t0,p_middle,internal_efficiency)
-        coef = coeff (pk,p_feed_water,t_feed_water,internal_efficiency,p_middle,t_middle,p0)
+        point2 = find_point2(p_middle, t_middle, p0, pk, internal_efficiency)
+        point_middle = find_point_middle(p_middle, t_middle, p0)
+        point_water = find_point_water(pk, p_feed_water, t_feed_water)
+        point0 = find_point0(p0, t0, p_middle)
+        point1 = find_point1(p0, t0, p_middle, internal_efficiency)
+        coef = coeff (pk, p_feed_water, t_feed_water, internal_efficiency, p_middle, t_middle, p0)
         numenator_without = point2[1].T * ( point_middle[0].s -  point_water[0].s)
         denumenator_without = (point0[1].h - point1[0].h) + (point_middle[1].h - point_water[0].h)
         without_part = 1 - (numenator_without / denumenator_without)
@@ -174,12 +174,12 @@ def complite_estimated_heat_drop(p0,
         :param float internal_efficiency: КПД цилиндров турбины
         :return Tuple[]: Значение теплоперепада, значение эффективности 
         """ 
-        point0 = find_point0(p0,t0,p_middle)
-        point1 = find_point1(p0,t0,p_middle,internal_efficiency)
-        point2 = find_point2(p_middle,t_middle,p0,pk,internal_efficiency)
-        point_middle = find_point_middle(p_middle,t_middle,p0)
-        point_water = find_point_water(pk,p_feed_water,t_feed_water)
-        ksi = complite_ksi_infinity(p0,t0,p_middle,t_middle,pk,p_feed_water,t_feed_water,internal_efficiency)
+        point0 = find_point0(p0, t0, p_middle)
+        point1 = find_point1(p0, t0, p_middle, internal_efficiency)
+        point2 = find_point2(p_middle, t_middle, p0, pk, internal_efficiency)
+        point_middle = find_point_middle(p_middle, t_middle,p0)
+        point_water = find_point_water(pk, p_feed_water, t_feed_water)
+        ksi = complite_ksi_infinity(p0, t0, p_middle, t_middle, pk, p_feed_water, t_feed_water, internal_efficiency)
         eff_num = ( point0[0].h - point1[0].h) *  internal_efficiency + (point_middle[0].h - point2[0].h) *  internal_efficiency
         eff_denum = ( point0[0].h - point1[0].h) *  internal_efficiency + (point_middle[1].h -  point_water[0].h)
         efficiency = (eff_num / eff_denum) * (1 / (1 - ksi))
@@ -261,7 +261,7 @@ def complite_condenser_mass_flow(p0,
                                                            t_feed_water,
                                                            internal_efficiency
                                                            )
-        point2 = find_point2(p_middle,t_middle,p0,pk,internal_efficiency)
+        point2 = find_point2(p_middle, t_middle, p0, pk, internal_efficiency)
         point_water = find_point_water(pk,p_feed_water,t_feed_water)
         condenser_mass_flow = (electrical_power / ((point2[1].h - point_water[0].h) * 1000 *  mechanical_efficiency *  generator_efficiency) * ((1 / estimated_heat_drop[1]) - 1))
         return condenser_mass_flow
